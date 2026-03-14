a93e3d6718b78d618293c29fadb5ed13365e7152
HMS159 <zaynmalik7866@gmail.com>
2026-03-14 12:26:44 +0530

    Minor Changes Done

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_BASE = import.meta.env.VITE_API_BASE_URL || "/api";
-const API_BASE = "/api"; // Temprary For Mobile Issue Fix
+const API_BASE = import.meta.env.DEV ? "/api" : (import.meta.env.VITE_API_BASE_URL || "/api");
+// const API_BASE = "/api"; // Temprary For Mobile Issue Fix
 
 const api = axios.create({
   baseURL: API_BASE,
