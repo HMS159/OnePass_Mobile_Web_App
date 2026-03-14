32c29c18ab83c67a26750e62ab8b93928ffe2459
HMS159 <zaynmalik7866@gmail.com>
2026-03-14 12:32:28 +0530

    minor changes dine

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes.jsx";
 
+const basename = import.meta.env.DEV ? "/" : "/user";
+
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <AppRoutes />
     </BrowserRouter>
   );
